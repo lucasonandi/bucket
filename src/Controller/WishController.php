8e393b52c3809979baf7e3a4bdacbfa8a831973f
lonandi2022 <lucas.onandi2022@campus-eni.fr>
2023-02-23 11:51:33 +0100

    tp- database

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,18 +14,27 @@ class WishController extends AbstractController
 
 #[
 Route('/list', name: 'list')]
-    public function list(): Response
+    public function list(WishRepository $wishRepository) : Response
     {
 
-        //TODO recuperer la liste des series en BDD
-        return $this->render('serie/list.html.twig');
+    $wishes = $wishRepository->orderByDate();
+    dump($wishes);
+        //TODO recuperer la liste des wish en BDD
+        return $this->render('wish/list.html.twig', ['wishes' => $wishes]);
     }
     #[Route('/{id}', name: 'detail', requirements: ['id' => '\d+'])]
-    public function show(int $id): Response
+    public function show(int $id, WishRepository $wishRepository): Response
     {
+        $wish = $wishRepository->find($id);
+        dump($id);
+
+        if (!$wish){
+            //lance une error 404 si la serie n'existe pas
+            throw $this->createNotFoundException("Oops ! wish not found !");
+        }
         dump($id);
 
         //TODO recupereration des infos de la serie en BDD
-        return $this->render('serie/detail.html.twig');
+        return $this->render('wish/detail.html.twig', ['wish'=>$wish]);
     }
 }
